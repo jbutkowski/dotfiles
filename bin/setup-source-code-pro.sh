83a5df70baf3fa8783390c0c237a68cdff5d805d
Justin Butkowski <jbutkowski@gmail.com>
2014-08-20 06:12:30 +1000

    gitcofig settings

--- bin/setup-source-code-pro.sh
+++ bin/setup-source-code-pro.sh
@@ -5,5 +5,5 @@ set -e
 cd $(mktemp -d)
 
 wget http://sourceforge.net/projects/sourcecodepro.adobe/files/SourceCodePro_FontsOnly-1.017.zip/download
-unzip -od /home/jma/.fonts/ download SourceCodePro_FontsOnly-1.017/OTF/\*
+unzip -od ~/.fonts/ download SourceCodePro_FontsOnly-1.017/OTF/\*
 fc-cache -vf ~/.fonts
