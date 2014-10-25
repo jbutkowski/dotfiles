9e049060eb8b61c3020e9f37d658c2436a3d2fb6
Justin Butkowski <jbutkowski@gmail.com>
2014-10-25 19:20:00 +1000

    rvm tweaks

--- shell/path.sh
+++ shell/path.sh
@@ -1,4 +1,4 @@
- PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+# PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
 function prepend_path() {
   if [ -d "$1" ]; then
@@ -14,12 +14,16 @@ function append_path() {
 PATH=$(getconf PATH)
 
 prepend_path '/usr/sbin'
+prepend_path '/usr/sbin'
 prepend_path '/usr/local/bin'
+prepend_path '/usr/local/sbin'
 prepend_path '~/.rvm/bin'
 prepend_path '~/bin'
-append_path '~/.rvm/bin'
+append_path  '~/.rvm/bin'
+
+prepend_path '/usr/local/opt/coreutils/libexec/gnubin'
 
 unset prepend_path
 unset append_path
 
- [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
