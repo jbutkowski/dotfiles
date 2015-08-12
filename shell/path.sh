6877c54c68651a6251d6e5ef651694f7619e9910
Justin Butkowski <jbutkowski@gmail.com>
2015-08-13 05:46:33 +1000

    add google cloud SDK source

--- shell/path.sh
+++ shell/path.sh
@@ -1,5 +1,11 @@
 # PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
+# The next line updates PATH for the Google Cloud SDK.
+source '~/google-cloud-sdk/path.bash.inc'
+
+# The next line enables shell command completion for gcloud.
+source '~/google-cloud-sdk/completion.bash.inc'
+
 function prepend_path() {
   if [ -d "$1" ]; then
     PATH="$1:$PATH"
