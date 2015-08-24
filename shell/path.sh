b2759b90fbe51fe188101a415c593b769a2eb30a
Justin Butkowski <jbutkowski@gmail.com>
2015-08-25 08:20:21 +1000

    WIP

--- shell/path.sh
+++ shell/path.sh
@@ -20,7 +20,7 @@ prepend_path '/usr/local/sbin'
 prepend_path '~/.rvm/bin'
 prepend_path '~/bin'
 append_path  '~/.rvm/bin'
-append_path  '~/.node/bin'
+#append_path  '~/.node/bin'
 prepend_path '/usr/local/opt/coreutils/libexec/gnubin'
 
 unset prepend_path
@@ -32,12 +32,12 @@ source '/Users/jbutkowski/google-cloud-sdk/path.bash.inc'
 # The next line enables shell command completion for gcloud.
 source '/Users/jbutkowski/google-cloud-sdk/completion.bash.inc'
 
-NPM_PACKAGES="${HOME}/.npm-packages"
-NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-PATH="$NPM_PACKAGES/bin:$PATH"
+#NPM_PACKAGES="${HOME}/.npm-packages"
+#NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+#PATH="$NPM_PACKAGES/bin:$PATH"
 # Unset manpath so we can inherit from /etc/manpath via the `manpath`
 # command
-unset MANPATH # delete if you already modified MANPATH elsewhere in your config
-MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+#unset MANPATH # delete if you already modified MANPATH elsewhere in your config
+#MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
