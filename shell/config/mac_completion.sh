6b0807175950c7057238e0ddc0f8825325442cc5
Justin Butkowski <jbutkowski@gmail.com>
2016-09-02 07:58:12 +1000

    add jenv config

--- shell/config/mac_completion.sh
+++ shell/config/mac_completion.sh
@@ -15,3 +15,5 @@ done
 if type _git &> /dev/null && [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
   complete -o default -o nospace -F _git g;
 fi;
+
+[[ -r $NVM_DIR/bash_completion  ]] && . $NVM_DIR/bash_completion
