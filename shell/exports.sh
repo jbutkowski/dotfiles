2752fd4c18ef2c9f7a660ef20802763e280e7c61
Justin Butkowski <jbutkowski@gmail.com>
2020-12-24 08:41:29 +1000

    update nvm paths

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,3 +2,4 @@ export PKG_CONFIG_PATH=/usr/local/opt/zlib/lib/pkgconfig:$PKG_CONFIG_PATH
 export PKG_CONFIG_PATH=/usr/local/opt/ncurses/lib/pkgconfig:$PKG_CONFIG_PATH
 #export PKG_CONFIG_PATH=/usr/local/opt/openssl@1.1/lib/pkgconfig:$PKG_CONFIG_PATH
 export PKG_CONFIG_PATH=/usr/local/opt/imagemagick@6/lib/pkgconfig:$PKG_CONFIG_PATH
+#export PKG_CONFIG_PATH=/usr/local/opt/icu4c/lib/pkgconfig:$PKG_CONFIG_PATH
